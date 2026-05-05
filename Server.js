6831587d4a487129e35bb5df76b36fea5b20fff2
Krunal <nsundersia@gmail.com>
2026-05-05 12:17:52 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(
   cors({
-    origin: "https://frontdata.vercel.app", // Use your specific fro
+    origin: "https://frontdata.vercel.app", // Use your specific f
     methods: ["GET", "POST", "DELETE", "OPTIONS"], // Added OPTIONS 
     credentials: true,
   })
