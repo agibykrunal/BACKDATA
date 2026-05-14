45ab95edd0356659cde17a89089ca6edebd07695
Krunal <nsundersia@gmail.com>
2026-05-14 16:40:24 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -18,7 +18,7 @@ app.use(
 app.use(express.json());
 
 // ─────────────────────────────────────────────
-//  PostgreSQL Pool (rea
+//  PostgreSQL Pool (re
 // ─────────────────────────────────────────────
 const pool = new Pool({
   host: process.env.PG_HOST,
